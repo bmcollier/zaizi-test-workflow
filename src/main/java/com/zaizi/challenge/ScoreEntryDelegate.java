93c641847b95663572f98b09c4d1a54489b71d4d
Ben Collier <bmcollier@gmail.com>
2020-11-24 11:45:01 +0000

    Added better logging

--- src/main/java/com/zaizi/challenge/ScoreEntryDelegate.java
+++ src/main/java/com/zaizi/challenge/ScoreEntryDelegate.java
@@ -2,13 +2,16 @@ package com.zaizi.challenge;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ScoreEntryDelegate implements JavaDelegate {
 
     @Override
     public void execute(DelegateExecution execution) throws Exception {
         String score = execution.getVariable("score").toString();
-        System.out.println(score);
+        Logger logger = LoggerFactory.getLogger(ScoreEntryDelegate.class);
+        logger.info("Score: {}", score);
     }
 
 }
